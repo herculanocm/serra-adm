5893e637a55de579fadc832aa0f3a22d16ecd4b5
Herculano Cunha <herculano.cunha2@gmail.com>
2017-03-23 22:18:04 -0300

    primeiro projeto spring boot, iniciando com hello word

---
+++ src/main/java/com/cunha/serra/SerraApplication.java
@@ -0,0 +1,12 @@
+package com.cunha.serra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerraApplication.class, args);
+	}
+}
